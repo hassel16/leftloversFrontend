7d3995a5f09451ca50b379e13e7a837e19e27697
Daniel Hasselwander <daniel.hasselwander@nttdata.com>
2018-03-08 12:17:47 +0100

    sushi

--- components/Statefull/Login.js
+++ components/Statefull/Login.js
@@ -1,4 +1,5 @@
 import { Component } from "react"
+import LoginModel from "./LoginModel"
 class Login extends Component {
     constructor(props) {
         super(props)
@@ -9,13 +10,18 @@ class Login extends Component {
 
         const {_username, _password} = this.refs
         var xhttp = new XMLHttpRequest();
-        if (this.readyState == 4 && this.status == 200) {
-           document.getElementById("root").innerHTML = this.responseText;
-          }
-        
-        let jdon = ``
-        xhttp.open("GET", "http://localhost/OpenWeatherAPIService/test2/", true);
-        xhttp.send();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+               // Typical action to be performed when the document is ready:
+               alert(xhttp.responseText);
+            }else{
+                alert(this.status)
+            }
+        };
+        let login = new Login(_username.value,_password.value)
+        xhttp.open("POST", "http://localhost/OpenWeatherAPIService/test2?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJsZWZ0bG92ZXJzX3d3aTE2QjMiLCJpYXQiOjE1MjA1MDYzODF9.FV3_c-XICWAt1TcAjJB9Z0OVbchjCbFUWfhSBcZtJ2w", true);
+        xmlhttp.setRequestHeader("Content-Type", "application/json");
+        xhttp.send(JSON.stringify(login));
         /*
         let ajax = new XMLHttpRequest()
         alert(`Username: ${_username.value}\nPassword: ${_password.value}`)
