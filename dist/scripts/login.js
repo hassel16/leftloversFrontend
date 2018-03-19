192bd422b61dd7dcdaee991b6dd220db3145e190
Jannik <36240897+itsJannik@users.noreply.github.com>
2018-03-19 16:50:32 +0100

    KeyListener-Enter hinzugefügt

--- dist/scripts/login.js
+++ dist/scripts/login.js
@@ -1,31 +1,41 @@
 import User from '../../data/User'
 import fetch from 'isomorphic-fetch'
 
+const login = () => {
+    const input_user = document.getElementById("login_benutzer")
+    const input_password = document.getElementById("login_passwort")
+
+    console.log(JSON.stringify(new User(input_user.value, input_password.value)))
+    fetch("https://leftloversgateway.azurewebsites.net/UAAService/login", { //oder andere url
+        method: "POST",
+        body: (new User(input_user.value, input_password.value)), //JSON.stringify
+    
+        //JSON.stringify(new User(input_user.value, input_password.value)),
+        headers: {
+            "Content-Type": "application/json"
+        }
+        
+    })
+    .then(response => response.json())
+    .then(responseJson => {
+        console.log("responsetext: " + JSON.stringify(responseJson))
+        return responseJson;
+    })
+    .catch(error => {
+      return console.log(error);
+    })
+
+}
+
 window.addEventListener("load", () => {
     const button_anmelden = document.getElementById("anmelde_button")
-    button_anmelden.addEventListener("click", () => {
-        const input_user = document.getElementById("login_benutzer")
-        const input_password = document.getElementById("login_passwort")
+    const last_input = document.getElementById("login_passwort")
 
-        console.log(JSON.stringify(new User(input_user.value, input_password.value)))
-        fetch("https://leftloversgateway.azurewebsites.net/UAAService/login", { //oder andere url
-            method: "POST",
-            body: (new User(input_user.value, input_password.value)), //JSON.stringify
-        
-            //JSON.stringify(new User(input_user.value, input_password.value)),
-            headers: {
-                "Content-Type": "application/json"
-            }
-            
-        })
-        .then(response => response.json())
-        .then(responseJson => {
-            console.log("responsetext: " + JSON.stringify(responseJson))
-            return responseJson;
-        })
-        .catch(error => {
-          return console.log(error);
-        })
-
+    button_anmelden.addEventListener("click", () => login())
+    last_input.addEventListener("keypress", (e) => {
+        const key = e.keyCode;
+        if (key === 13) { // 13 is enter
+          login()
+        }
     })
 })
--- dist/scripts/signin.js
+++ dist/scripts/signin.js
@@ -4,9 +4,115 @@ import City from '../../data/City'
 import fetch from 'isomorphic-fetch'
 //key: AIzaSyBUaYI1y3ig_ZVp5C57Sr633U7kl5Bnk0s
 
+var global = undefined
+
+const signup = () => {
+    const input_city = document.getElementById("city")
+    const input_email = document.getElementById("email")
+    const input_user = document.getElementById("uname")
+    const input_password = document.getElementById("psw")
+    const input_password_repeat = document.getElementById("psw_repeat")
+
+
+    let register = new Register("invalid city", input_email.value, input_user.value, input_password.value, input_password_repeat.value)
+    console.log("frisch erzeugtes Registerobjekt: " + JSON.stringify(register))
+    const create_div = (big_sister, textContent) => {
+        if (big_sister.nextElementSibling.nodeName !== "DIV") {
+            let user_error_div = document.createElement("div")
+            user_error_div.classList.add("error_div")
+            user_error_div.setAttribute("id", textContent)
+            user_error_div.textContent = textContent
+            big_sister.parentNode.insertBefore(user_error_div, big_sister.nextSibling)
+        }
+    }
+
+    const remove_div = id => {
+        if (document.getElementById(id)) {
+            document.getElementById(id).parentNode.removeChild(document.getElementById(id))
+        }
+    }
+
+
+
+    if (!input_city.value || input_city.value === "") {
+        register.setFlag()
+        create_div(input_city, "! Keine Stadt gesucht")
+    } else {
+        remove_div("! Keine Stadt gesucht")
+        if (global === undefined || global === "") {
+            register.setFlag()
+            create_div(input_city, "! Keine Stadt aus den Vorschlägen ausgewählt")
+        } else {
+            remove_div("! Keine Stadt aus den Vorschlägen ausgewählt")
+            const long_name = global.address_components.filter(x => x.types[0] === "locality")[0].long_name // :)
+            const lat = global.geometry.location.lat() //entweder stringify oder toString!
+            const lng = global.geometry.location.lng()
+            register.city = new City(long_name, lat, lng)
+            console.log("stadt mit stadtobjekt" + JSON.stringify(register))
+        }
+    }
+
+
+    if (!register.check_userName()) {
+        register.setFlag()
+        create_div(input_user, "! Ungültiger Benutzername")
+        input_user.value = ""
+    } else {
+        remove_div("! Ungültiger Benutzername")
+    }
+
+    if (!register.check_email()) {
+        register.setFlag()
+        create_div(input_email, "! Ungültige E-Mail Adresse")
+    } else {
+        remove_div("! Ungültige E-Mail Adresse")
+    }
+
+    if (!register.check_passwordLength()) {
+        register.setFlag()
+        create_div(input_password, "! Ungültiges Passwort")
+        input_password.value = ""
+        input_password_repeat.value = ""
+    } else {
+        remove_div("! Ungültiges Passwort")
+    }
+
+    if (!register.same_passwords()) {
+        register.setFlag()
+        create_div(input_password_repeat, "! Passwörter sind nicht identisch")
+        input_password.value = ""
+        input_password_repeat.value = ""
+    } else {
+        remove_div("! Passwörter sind nicht identisch")
+    }
+
+    if (register.flag) {
+        alert("alles fine")
+        fetch("https://leftloversgateway.azurewebsites.net/UAAService/signup", { //oder andere url
+            method: "POST",
+            body: register,
+
+            headers: {
+                "Content-Type": "application/json"
+            }
+
+        })
+            .then(response => {
+                return response.json()
+            })
+            .then(responseJson => {
+                console.log("responsetext: " + JSON.stringify(responseJson))
+                return responseJson;
+            })
+            .catch(error => {
+                return console.error(error);
+            })
+
+    }
+}
 
 window.addEventListener("load", () => {
-    var global = undefined
+ 
     const acc = new google.maps.places.Autocomplete(document.getElementById("city"), {
         //types: ['(cities)'],
         componentRestrictions: { country: 'de' }
@@ -17,108 +123,13 @@ window.addEventListener("load", () => {
         global = place
     })
     const button_registrieren = document.getElementById("submit_button")
-    button_registrieren.addEventListener("click", () => {
-        const input_city = document.getElementById("city")
-        const input_email = document.getElementById("email")
-        const input_user = document.getElementById("uname")
-        const input_password = document.getElementById("psw")
-        const input_password_repeat = document.getElementById("psw_repeat")
+    button_registrieren.addEventListener("click", () => signup())
 
-
-        let register = new Register("invalid city", input_email.value, input_user.value, input_password.value, input_password_repeat.value)
-        console.log("frisch erzeugtes Registerobjekt: " + JSON.stringify(register))
-        const create_div = (big_sister, textContent) => {
-            if (big_sister.nextElementSibling.nodeName !== "DIV") {
-                let user_error_div = document.createElement("div")
-                user_error_div.classList.add("error_div")
-                user_error_div.setAttribute("id", textContent)
-                user_error_div.textContent = textContent
-                big_sister.parentNode.insertBefore(user_error_div, big_sister.nextSibling)
-            }
-        }
-
-        const remove_div = id => {
-            if (document.getElementById(id)) {
-                document.getElementById(id).parentNode.removeChild(document.getElementById(id))
-            }
-        }
-
-
-
-        if (!input_city.value || input_city.value === "") {
-            register.setFlag()
-            create_div(input_city, "! Keine Stadt gesucht")
-        } else {
-            remove_div("! Keine Stadt gesucht")
-            if (global === undefined || global === "") {
-                register.setFlag()
-                create_div(input_city, "! Keine Stadt aus den Vorschlägen ausgewählt")
-            } else {
-                remove_div("! Keine Stadt aus den Vorschlägen ausgewählt")
-                const long_name = global.address_components.filter(x => x.types[0] === "locality")[0].long_name // :)
-                const lat = global.geometry.location.lat() //entweder stringify oder toString!
-                const lng = global.geometry.location.lng()
-                register.city = new City(long_name, lat, lng)
-                console.log("stadt mit stadtobjekt" + JSON.stringify(register))
-            }
-        }
-
-
-        if (!register.check_userName()) {
-            register.setFlag()
-            create_div(input_user, "! Ungültiger Benutzername")
-            input_user.value = ""
-        } else {
-            remove_div("! Ungültiger Benutzername")
-        }
-
-        if (!register.check_email()) {
-            register.setFlag()
-            create_div(input_email, "! Ungültige E-Mail Adresse")
-        } else {
-            remove_div("! Ungültige E-Mail Adresse")
-        }
-
-        if (!register.check_passwordLength()) {
-            register.setFlag()
-            create_div(input_password, "! Ungültiges Passwort")
-            input_password.value = ""
-            input_password_repeat.value = ""
-        } else {
-            remove_div("! Ungültiges Passwort")
-        }
-
-        if (!register.same_passwords()) {
-            register.setFlag()
-            create_div(input_password_repeat, "! Passwörter sind nicht identisch")
-            input_password.value = ""
-            input_password_repeat.value = ""
-        } else {
-            remove_div("! Passwörter sind nicht identisch")
-        }
-
-        if (register.flag) {
-            alert("alles fine")
-            fetch("https://leftloversgateway.azurewebsites.net/UAAService/signup", { //oder andere url
-                method: "POST",
-                body: register,
-
-                headers: {
-                    "Content-Type": "application/json"
-                }
-
-            })
-                .then(response => {
-                    return response.json()
-                })
-                .then(responseJson => {
-                    console.log("responsetext: " + JSON.stringify(responseJson))
-                    return responseJson;
-                })
-                .catch(error => {
-                    return console.error(error);
-                })
-
+    const last_input = document.getElementById("psw_repeat")
+    last_input.addEventListener("keypress", (e) => {
+        const key = e.keyCode;
+        if (key === 13) { // 13 is enter
+          signup()
         }
     })
 })
