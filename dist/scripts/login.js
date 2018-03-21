81f5ee47a42830525af0145347aaf18207057284
Jannik <36240897+itsJannik@users.noreply.github.com>
2018-03-21 22:36:06 +0100

    leerer user/passwort wirft fehler

--- dist/scripts/login.js
+++ dist/scripts/login.js
@@ -3,27 +3,42 @@ import {postRequest} from '../../data/APICall'
 import {create_div, remove_div} from '../../data/Factory'
 
 const login = () => {
+    let sauber = true
     const input_user = document.getElementById("login_benutzer")
     const input_password = document.getElementById("login_passwort")
 
-    postRequest("UAAService/login", JSON.stringify(new User(input_user.value, input_password.value))) 
-    .then(response => {
-        console.log("headers: " + response.headers.get("authorization"))
-        if (response.status !== 200) {
-            create_div(input_password, "! Benutzername oder Passwort falsch")
-        } else {
-            remove_div("! Benutzername oder Passwort falsch")
-        }
-        return response.json()
-    })
-    .then(responseJson => {
-        console.log("responsetext: " + JSON.stringify(responseJson))
-        return responseJson;
-    })
-    .catch(error => {
-      return console.log(error);
-    })
-    
+    if (input_user.value === "" || !input_user.value) {
+        create_div(input_user, "! Gib einen Benutzernamen an")
+        sauber = false
+    } else {
+        remove_div("! Gib einen Benutzernamen an")
+    }
+
+    if (input_password.value === "" || !input_password.value) {
+        create_div(input_password, "! Gib ein Passwort an")
+        sauber = false
+    } else {
+        remove_div("! Gib ein Passwort an")
+    }
+    if (sauber) {
+        postRequest("UAAService/login", JSON.stringify(new User(input_user.value, input_password.value))) 
+        .then(response => {
+            console.log("headers: " + response.headers.get("authorization"))
+            if (response.status !== 200) {
+                create_div(input_password, "! Benutzername oder Passwort falsch")
+            } else {
+                remove_div("! Benutzername oder Passwort falsch")
+            }
+            return response.json()
+        })
+        .then(responseJson => {
+            console.log("responsetext: " + JSON.stringify(responseJson))
+            return responseJson;
+        })
+        .catch(error => {
+          return console.log(error);
+        })
+    }
 }
 
 window.addEventListener("load", () => {
